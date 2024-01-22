10830be0247e274c05e4dcadf225295baacd7af4
thalissongoncalves <thalissongsilva70@gmail.com>
2024-01-22 17:48:14 -0300

    Feito seção de Projetos

---
+++ js/slick-carrousel.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.main-projects-cards-carousel').slick({
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  });
