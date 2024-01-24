52e2efffb1758f82fef02a3f32b84148323ceef0
thalissongoncalves <thalissongsilva70@gmail.com>
2024-01-24 17:45:54 -0300

    Criando a lógica para filtragem de projetos

--- js/btn-project-filter.js
+++ js/btn-project-filter.js
@@ -33,8 +33,82 @@ projects.map((project) => {
 });
 
 projectsData.map((project) => {
-    console.log(project)
+    const mainProjectCard = document.createElement("div");
+    mainProjectCard.className = "main-project-card";
+    const mainProjectCardTwoColumns = document.createElement("div");
+    mainProjectCardTwoColumns.className = "main-project-card-two-columns";
+    const mainProjectImage = document.createElement("div");
+    mainProjectImage.className = "main-project-image";
+    const projectImageUrl = document.createElement("img");
+    projectImageUrl.src = project.image;
+    projectImageUrl.alt = project.alt;
+    projectImageUrl.style.borderRadius = "5px";
+    projectImageUrl.style.width = "100%";
+    const mainProjectInfo = document.createElement("div");
+    mainProjectInfo.className = "main-project-info";
+    mainProjectInfo.style.paddingLeft = "20px";
+    const mainProjectInfoTitle = document.createElement("h2");
+    mainProjectInfoTitle.innerText = project.title;
+    const mainProjectInfoDescription = document.createElement("p");
+    mainProjectInfoDescription.innerText = project.description;
+    const mainProjectBtns = document.createElement("div");
+    mainProjectBtns.className = "main-project-btns";
+    const mainProjectButtonOne = document.createElement("button");
+    const mainProjectButtonOneIcon = document.createElement("i");
+    mainProjectButtonOneIcon.classList = "fa-brands fa-github";
+    mainProjectButtonOne.appendChild(mainProjectButtonOneIcon);
+    const mainProjectButtonOneText = document.createElement("span");
+    mainProjectButtonOneText.innerText = " GitHub";
+    mainProjectButtonOne.appendChild(mainProjectButtonOneText);
+    const mainProjectButtonSecond = document.createElement("button");
+    const mainProjectButtonSecondIcon = document.createElement("i");
+    mainProjectButtonSecondIcon.classList = "fa-solid fa-link";
+    mainProjectButtonSecond.appendChild(mainProjectButtonSecondIcon);
+    const mainProjectButtonSecondText = document.createElement("span");
+    mainProjectButtonSecondText.innerText = " Deploy";
+    mainProjectButtonSecond.appendChild(mainProjectButtonSecondText);
+    projectsCardsCarousel.appendChild(mainProjectCard);
+    mainProjectCard.appendChild(mainProjectCardTwoColumns);
+    mainProjectCardTwoColumns.appendChild(mainProjectImage);
+    mainProjectImage.appendChild(projectImageUrl);
+    mainProjectCardTwoColumns.appendChild(mainProjectInfo);
+    mainProjectInfo.appendChild(mainProjectInfoTitle);
+    mainProjectInfo.appendChild(mainProjectInfoDescription);
+    mainProjectCardTwoColumns.appendChild(mainProjectBtns);
+    mainProjectBtns.appendChild(mainProjectButtonOne);
+    mainProjectBtns.appendChild(mainProjectButtonSecond);
+    const mainProjectCardTechnologies = document.createElement("div");
+    
 });
+
+{/* <div class="main-project-card">
+        <div class="main-project-card-two-columns">
+
+            <div class="main-project-image">
+                <img src="./images/project-one.png" alt="imagem do projeto Naya Outlet" style="border-radius: 5px; width: 100%"/>
+            </div>
+
+            <div class="main-project-info" style="padding-left: 20px;">
+                <h2>01 - Naya Outlet</h2>
+                <p>Loja de roupas com foco em vendas online. O projeto foi feito em WordPress. Todo o projeto foi feito do zero. "Site"</p>
+            </div>
+
+            <div class="main-project-btns">
+                <button><i class="fa-brands fa-github"></i> GitHub</button>
+                <button><i class="fa-solid fa-link"></i> Deploy</button>
+            </div>
+
+            <div class="main-project-card-technologies">
+                <div style="display: flex; flex-direction: row; justify-content: right;">
+                    <img width="32" height="32" src="https://img.icons8.com/color/32/wordpress.png" alt="wordpress"/>
+                    <img width="32" height="32" src="https://img.icons8.com/color/48/html-5--v1.png" alt="html-5--v1"/>
+                    <img width="32" height="32" src="https://img.icons8.com/color/48/css3.png" alt="css3"/>
+                    <img width="32" height="32" src="https://img.icons8.com/plasticine/100/react.png" alt="react"/>
+                </div>
+            </div>
+
+        </div>
+    </div> */}
 
 const filterBtns = document.querySelector(".main-projects-buttons-filter").children;
 const filterBtnsArray = Object.entries(filterBtns).map(([chave, valor]) => valor);
@@ -57,26 +131,3 @@ filterBtnsArray.map((btn) => {
     });
 });
 
-{/* <div class="main-project-card">
-    <div class="main-project-card-two-columns">
-        <div>
-            <img src="./images/project-one.png" alt="imagem do projeto Naya Outlet" style="border-radius: 5px; width: 100%"/>
-        </div>
-        <div style="padding-left: 20px;">
-            <h2>01 - Naya Outlet</h2>
-            <p>Loja de roupas com foco em vendas online. O projeto foi feito em WordPress. Todo o projeto foi feito do zero. "Site"</p>
-        </div>
-        <div class="main-project-btns">
-            <button><i class="fa-brands fa-github"></i> GitHub</button>
-            <button><i class="fa-solid fa-link"></i> Deploy</button>
-        </div>
-        <div class="main-project-card-technologies">
-            <div style="display: flex; flex-direction: row; justify-content: right;">
-                <img width="32" height="32" src="https://img.icons8.com/color/32/wordpress.png" alt="wordpress"/>
-                <img width="32" height="32" src="https://img.icons8.com/color/48/html-5--v1.png" alt="html-5--v1"/>
-                <img width="32" height="32" src="https://img.icons8.com/color/48/css3.png" alt="css3"/>
-                <img width="32" height="32" src="https://img.icons8.com/plasticine/100/react.png" alt="react"/>
-            </div>
-        </div>
-    </div>
-</div> */}
--- js/projects.js
+++ js/projects.js
@@ -1,9 +1,10 @@
 const projectsData = [
     {
-        name: "01 - Lessons Learned",
-        description: "Foi desenvolvido um site que contém uma série de informações sobre o que aprendi na Trybe ao longo das primeiras aulas de Fundamentos. O site contém elementos posicionados e estilizados, contendo semântica apropriada para que seja acessível e melhor ranqueado.",
+        title: "01 - Lessons Learned",
+        description: "Foi desenvolvido um site que contém uma série de informações sobre o que aprendi na Trybe ao longo das primeiras aulas de Fundamentos.",
         technologies: ["HTML", "CSS", "TODOS"],
         githubUrl: "https://github.com/thalissongoncalves/Project-Lessons-Learned/tree/main/Lessons-learned",
+        alt: "Imagem Demo do projeto Lessons Learned",
         deployUrl: "",
         image: "./images/lesson-learned-project-img.png",
     },
