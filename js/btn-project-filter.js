0dee95311ef0d0ee8a72b0d6eff282a16bb1fc55
Thalisson Goncalves da Silva <thalissongsilva70@gmail.com>
2024-01-24 23:09:03 -0300

    Feito implementação dos cards, agora ajustando para que ao clicar nos filtros puxar os cards com as techs desejadas

--- js/btn-project-filter.js
+++ js/btn-project-filter.js
@@ -1,8 +1,42 @@
 import projectsData from "./projects.js";
 
 const filtersBtnBlock = document.querySelector(".main-projects-buttons-filter");
-const projects = ["TODOS", "HTML", "CSS", "JAVASCRIPT", "TYPESCRIPT", "PYTHON", "REACT", "WORDPRESS"];
+const projects = ["HTML", "CSS", "JAVASCRIPT", "REACT", "TYPESCRIPT", "PYTHON", "MYSQL", "WORDPRESS"];
 const projectsCardsCarousel = document.querySelector(".main-projects-cards-carousel");
+const projectsIcon = [
+    {
+        tech: "HTML",
+        src: "https://img.icons8.com/color/48/html-5--v1.png",
+    },
+    {
+        tech: "CSS",
+        src: "https://img.icons8.com/color/48/css3.png",
+    },
+    {
+        tech: "JAVASCRIPT",
+        src: "https://img.icons8.com/color/48/javascript--v1.png",
+    },
+    {
+        tech: "TYPESCRIPT",
+        src: "https://img.icons8.com/color/48/typescript.png",
+    },
+    {
+        tech: "PYTHON",
+        src: "https://img.icons8.com/color/48/python--v1.png",
+    },
+    {
+        tech: "REACT",
+        src: "https://img.icons8.com/plasticine/100/react.png",
+    },
+    {
+        tech: "MYSQL",
+        src: "https://img.icons8.com/color/48/mysql-logo.png",
+    },
+    {
+        tech: "WORDPRESS",
+        src: "https://img.icons8.com/color/32/wordpress.png",
+    },
+];
 
 projects.map((project) => {
     const filtersBtn = document.createElement("button");
@@ -32,54 +66,78 @@ projects.map((project) => {
     
 });
 
-projectsData.map((project) => {
-    const mainProjectCard = document.createElement("div");
-    mainProjectCard.className = "main-project-card";
-    const mainProjectCardTwoColumns = document.createElement("div");
-    mainProjectCardTwoColumns.className = "main-project-card-two-columns";
-    const mainProjectImage = document.createElement("div");
-    mainProjectImage.className = "main-project-image";
-    const projectImageUrl = document.createElement("img");
-    projectImageUrl.src = project.image;
-    projectImageUrl.alt = project.alt;
-    projectImageUrl.style.borderRadius = "5px";
-    projectImageUrl.style.width = "100%";
-    const mainProjectInfo = document.createElement("div");
-    mainProjectInfo.className = "main-project-info";
-    mainProjectInfo.style.paddingLeft = "20px";
-    const mainProjectInfoTitle = document.createElement("h2");
-    mainProjectInfoTitle.innerText = project.title;
-    const mainProjectInfoDescription = document.createElement("p");
-    mainProjectInfoDescription.innerText = project.description;
-    const mainProjectBtns = document.createElement("div");
-    mainProjectBtns.className = "main-project-btns";
-    const mainProjectButtonOne = document.createElement("button");
-    const mainProjectButtonOneIcon = document.createElement("i");
-    mainProjectButtonOneIcon.classList = "fa-brands fa-github";
-    mainProjectButtonOne.appendChild(mainProjectButtonOneIcon);
-    const mainProjectButtonOneText = document.createElement("span");
-    mainProjectButtonOneText.innerText = " GitHub";
-    mainProjectButtonOne.appendChild(mainProjectButtonOneText);
-    const mainProjectButtonSecond = document.createElement("button");
-    const mainProjectButtonSecondIcon = document.createElement("i");
-    mainProjectButtonSecondIcon.classList = "fa-solid fa-link";
-    mainProjectButtonSecond.appendChild(mainProjectButtonSecondIcon);
-    const mainProjectButtonSecondText = document.createElement("span");
-    mainProjectButtonSecondText.innerText = " Deploy";
-    mainProjectButtonSecond.appendChild(mainProjectButtonSecondText);
-    projectsCardsCarousel.appendChild(mainProjectCard);
-    mainProjectCard.appendChild(mainProjectCardTwoColumns);
-    mainProjectCardTwoColumns.appendChild(mainProjectImage);
-    mainProjectImage.appendChild(projectImageUrl);
-    mainProjectCardTwoColumns.appendChild(mainProjectInfo);
-    mainProjectInfo.appendChild(mainProjectInfoTitle);
-    mainProjectInfo.appendChild(mainProjectInfoDescription);
-    mainProjectCardTwoColumns.appendChild(mainProjectBtns);
-    mainProjectBtns.appendChild(mainProjectButtonOne);
-    mainProjectBtns.appendChild(mainProjectButtonSecond);
-    const mainProjectCardTechnologies = document.createElement("div");
+const createProjectCards = (tech) => {
+    if (!tech) {
+        projectsData.map((project) => {
+            const mainProjectCard = document.createElement("div");
+            mainProjectCard.className = "main-project-card";
+            mainProjectCard.style.display = project.display;
+            const mainProjectCardTwoColumns = document.createElement("div");
+            mainProjectCardTwoColumns.className = "main-project-card-two-columns";
+            const mainProjectImage = document.createElement("div");
+            mainProjectImage.className = "main-project-image";
+            const projectImageUrl = document.createElement("img");
+            projectImageUrl.src = project.image;
+            projectImageUrl.alt = project.alt;
+            projectImageUrl.style.borderRadius = "5px";
+            projectImageUrl.style.width = "100%";
+            const mainProjectInfo = document.createElement("div");
+            mainProjectInfo.className = "main-project-info";
+            const mainProjectInfoTitle = document.createElement("h2");
+            mainProjectInfoTitle.innerText = project.title;
+            const mainProjectInfoDescription = document.createElement("p");
+            mainProjectInfoDescription.innerText = project.description;
+            const mainProjectBtns = document.createElement("div");
+            mainProjectBtns.className = "main-project-btns";
+            const mainProjectButtonOne = document.createElement("button");
+            const mainProjectButtonOneIcon = document.createElement("i");
+            mainProjectButtonOneIcon.classList = "fa-brands fa-github";
+            mainProjectButtonOne.appendChild(mainProjectButtonOneIcon);
+            const mainProjectButtonOneText = document.createElement("span");
+            mainProjectButtonOneText.innerText = " GitHub";
+            mainProjectButtonOne.appendChild(mainProjectButtonOneText);
+            const mainProjectButtonSecond = document.createElement("button");
+            const mainProjectButtonSecondIcon = document.createElement("i");
+            mainProjectButtonSecondIcon.classList = "fa-solid fa-link";
+            mainProjectButtonSecond.appendChild(mainProjectButtonSecondIcon);
+            const mainProjectButtonSecondText = document.createElement("span");
+            mainProjectButtonSecondText.innerText = " Deploy";
+            mainProjectButtonSecond.appendChild(mainProjectButtonSecondText);
+            projectsCardsCarousel.appendChild(mainProjectCard);
+            mainProjectCard.appendChild(mainProjectCardTwoColumns);
+            mainProjectCardTwoColumns.appendChild(mainProjectImage);
+            mainProjectImage.appendChild(projectImageUrl);
+            mainProjectCardTwoColumns.appendChild(mainProjectBtns);
+            mainProjectBtns.appendChild(mainProjectButtonOne);
+            mainProjectBtns.appendChild(mainProjectButtonSecond);
+            mainProjectCardTwoColumns.appendChild(mainProjectInfo);
+            mainProjectInfo.appendChild(mainProjectInfoTitle);
+            mainProjectInfo.appendChild(mainProjectInfoDescription);
+            const mainProjectCardTechnologies = document.createElement("div");
+            mainProjectCardTechnologies.className = "main-project-card-technologies";
+            mainProjectCardTwoColumns.appendChild(mainProjectCardTechnologies);
+            const mainProjectCardTechnologiesRow = document.createElement("div");
+            mainProjectCardTechnologiesRow.style.display = "flex";
+            mainProjectCardTechnologiesRow.style.flexDirection = "row";
+            mainProjectCardTechnologiesRow.style.justifyContent = "right";
+            mainProjectCardTechnologies.appendChild(mainProjectCardTechnologiesRow);
+            project.technologies.map((tech) => {
+                const mainProjectCardTechnologiesIcon = document.createElement("img");
+                mainProjectCardTechnologiesIcon.style.width = "32px";
+                mainProjectCardTechnologiesIcon.style.height = "32px";
+                projectsIcon.map((icon) => {
+                    if (icon.tech === tech) {
+                        mainProjectCardTechnologiesIcon.src = icon.src;
+                    };
+                });
+                mainProjectCardTechnologiesRow.appendChild(mainProjectCardTechnologiesIcon);
+            });
+        });
+    }
     
-});
+}
+
+
 
 {/* <div class="main-project-card">
         <div class="main-project-card-two-columns">
@@ -88,14 +146,14 @@ projectsData.map((project) => {
                 <img src="./images/project-one.png" alt="imagem do projeto Naya Outlet" style="border-radius: 5px; width: 100%"/>
             </div>
 
-            <div class="main-project-info" style="padding-left: 20px;">
-                <h2>01 - Naya Outlet</h2>
-                <p>Loja de roupas com foco em vendas online. O projeto foi feito em WordPress. Todo o projeto foi feito do zero. "Site"</p>
-            </div>
-
             <div class="main-project-btns">
                 <button><i class="fa-brands fa-github"></i> GitHub</button>
                 <button><i class="fa-solid fa-link"></i> Deploy</button>
+            </div>
+
+            <div class="main-project-info" style="padding-left: 20px;">
+                <h2>01 - Naya Outlet</h2>
+                <p>Loja de roupas com foco em vendas online. O projeto foi feito em WordPress. Todo o projeto foi feito do zero. "Site"</p>
             </div>
 
             <div class="main-project-card-technologies">
@@ -112,7 +170,8 @@ projectsData.map((project) => {
 
 const filterBtns = document.querySelector(".main-projects-buttons-filter").children;
 const filterBtnsArray = Object.entries(filterBtns).map(([chave, valor]) => valor);
-const filterBtnsSelected = []
+const filterBtnsSelected = [];
+
 
 filterBtnsArray.map((btn) => {
     btn.addEventListener("click", () => {
@@ -127,7 +186,13 @@ filterBtnsArray.map((btn) => {
                     filterBtnsSelected.splice(filterIndex, 1);
                 }
             })
-        }
+        };
+
     });
 });
 
+if (filterBtnsSelected.length === 0) {
+    console.log("Length == 0")
+} else {
+    console.log("Length > 1")
+};
--- js/projects.js
+++ js/projects.js
@@ -1,8 +1,21 @@
 const projectsData = [
     {
+        id: "01",
+        display: "block",
         title: "01 - Lessons Learned",
         description: "Foi desenvolvido um site que contém uma série de informações sobre o que aprendi na Trybe ao longo das primeiras aulas de Fundamentos.",
-        technologies: ["HTML", "CSS", "TODOS"],
+        technologies: ["HTML", "CSS"],
+        githubUrl: "https://github.com/thalissongoncalves/Project-Lessons-Learned/tree/main/Lessons-learned",
+        alt: "Imagem Demo do projeto Lessons Learned",
+        deployUrl: "",
+        image: "./images/lesson-learned-project-img.png",
+    },
+    {
+        id: "02",
+        display: "block",
+        title: "01 - Lessons Learned",
+        description: "Foi desenvolvido um site que contém uma série de informações sobre o que aprendi na Trybe ao longo das primeiras aulas de Fundamentos.",
+        technologies: ["JAVASCRIPT", "REACT"],
         githubUrl: "https://github.com/thalissongoncalves/Project-Lessons-Learned/tree/main/Lessons-learned",
         alt: "Imagem Demo do projeto Lessons Learned",
         deployUrl: "",
