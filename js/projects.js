820778198d05eee8c18bc0ad128d532d3392896b
thalissongoncalves <thalissongsilva70@gmail.com>
2024-01-25 17:47:00 -0300

    Feito alguns ajustes de css nos botões github e deploy

--- js/projects.js
+++ js/projects.js
@@ -21,6 +21,17 @@ const projectsData = [
         deployUrl: "",
         image: "./images/lesson-learned-project-img.png",
     },
+    {
+        id: "03",
+        display: "block",
+        title: "03 - Terceiro Projeto",
+        description: "Foi desenvolvido um site que contém uma série de informações sobre o que aprendi na Trybe ao longo das primeiras aulas de Fundamentos.",
+        technologies: ["JAVASCRIPT", "REACT"],
+        githubUrl: "https://github.com/thalissongoncalves/Project-Lessons-Learned/tree/main/Lessons-learned",
+        alt: "Imagem Demo do projeto Lessons Learned",
+        deployUrl: "",
+        image: "./images/lesson-learned-project-img.png",
+    },
 ]
 
 export default projectsData;
