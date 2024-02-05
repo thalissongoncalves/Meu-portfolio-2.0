4ccb82589f38a040d85f0a8ff38a242bc4a33f55
thalissongoncalves <thalissongsilva70@gmail.com>
2024-02-05 12:46:07 -0300

    Atualiza responsivo para celular

--- js/left-menu-mobile.js
+++ js/left-menu-mobile.js
@@ -4,12 +4,15 @@ const leftContent = document.querySelector(".left-content");
 const avatarBlock = document.querySelector(".avatar-block");
 const scrollableContent = document.querySelector("#scrollable-content");
 const avatarCloseBtn = document.querySelector(".avatar-block-close-btn");
+const closeBtn = document.querySelector(".btn-close-cell");
+const mainBlock = document.getElementById("home");
 
 avatarBtnMobile.addEventListener('click', function() {
     headerMobile.style.display = "none";
     leftContent.style.display = "block";
     avatarBlock.style.display = "flex";
     scrollableContent.style.display = "block";
+    mainBlock.style.display = "none";
 })
 
 avatarCloseBtn.addEventListener('click', function() {
@@ -17,4 +20,10 @@ avatarCloseBtn.addEventListener('click', function() {
     leftContent.style.display = "none";
     avatarBlock.style.display = "none";
     scrollableContent.style.display = "none";
+})
+
+closeBtn.addEventListener("click", function() {
+    leftContent.style.display = "none";
+    headerMobile.style.display = "block";
+    // mainBlock.style.display = "block";
 })
--- js/right-menu-mobile.js
+++ js/right-menu-mobile.js
@@ -2,13 +2,16 @@ const rightMenuBtn = document.querySelector(".menu-right-btn");
 const mobileHeader = document.querySelector(".avatar-cell-container");
 const menuBlock = document.querySelector("#menu-block");
 const menuBtnMobile = document.querySelector(".menu-btn-mobile");
+// const mainBlock = document.getElementById("home");
 
 rightMenuBtn.addEventListener('click', function() {
     mobileHeader.style.display = "none";
     menuBlock.style.display = "block";
+    mainBlock.style.display = "none";
 });
 
 menuBtnMobile.addEventListener('click', function() {
     menuBlock.style.display = "none";
     mobileHeader.style.display = "block";
+    mainBlock.style.display = "block";
 })
