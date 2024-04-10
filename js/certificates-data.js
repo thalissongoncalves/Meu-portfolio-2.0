d354b0fa9da3b98a2417d980b4baaf77841b5cb6
thalissongoncalves <thalissongsilva70@gmail.com>
2024-04-09 23:51:54 -0300

    .

--- js/certificates-data.js
+++ js/certificates-data.js
@@ -1,64 +1,10 @@
 const certificateData = [
     {
-        id: "10",
-        title: "Ciência da Computação",
-        credencial: "https://www.credential.net/8af89811-8c91-4506-a732-70484cde28a1",
-        image: "./images/Certificado-10.png",
-    },
-    {
-        id: "09",
-        title: "Desenvolvimento em Back-end",
-        credencial: "https://www.credential.net/a498e3bb-dc4e-4fb2-8411-8e33f9a7806a#gs.3wwv8m",
-        image: "./images/Certificado-09.png",
-    },
-    {
-        id: "08",
-        title: "Linguagem de Programação Python - Básico",
-        credencial: "https://www.ev.org.br/validar-certificado",
-        image: "./images/Certificado-08.jpeg",
-    },
-    {
-        id: "07",
-        title: "Fundamentos de Lógica de Programação",
-        credencial: "https://www.ev.org.br/validar-certificado",
-        image: "./images/Certificado-07.jpeg",
-    },
-    {
-        id: "06",
-        title: "Introdução à Programação Orientada a Objetos (POO)",
-        credencial: "https://www.ev.org.br/validar-certificado",
-        image: "./images/Certificado-06.jpeg",
-    },
-    {
-        id: "05",
-        title: "Front-End",
-        credencial: "https://www.credential.net/4660de7e-1e99-4e92-8229-b441b70f1eac#gs.3wt8gt",
-        image: "./images/Certificado-05.png",
-    },
-    {
-        id: "04",
-        title: "Fundamentos do Desenvolvimento Web",
-        credencial: "https://www.credential.net/8d942894-b634-4ee0-937f-7b151dded748#gs.3wt242",
-        image: "./images/Certificado-04.png",
-    },
-    {
-        id: "03",
-        title: "WORDPRESS COM GUTENBERG - MÓDULO 02: SEGURANÇA E PERFORMANCE",
-        credencial: "https://drive.google.com/file/d/1U2kekdhLY9N2M5JZrnCwJk3vkxNeUbCT/view",
-        image: "./images/Certificado-03.png",
-    },
-    {
-        id: "02",
-        title: "WORDPRESS COM GUTENBERG - MÓDULO 01: CRIAÇÃO DO SITE",
-        credencial: "https://drive.google.com/file/d/1U2TU1OQUYcb1xOCpfCZmtulu3Rl5X9Px/view",
-        image: "./images/Certificado-02.png",
-    },
-    {
         id: "01",
-        title: "CURSO HTML5 E CSS3: MÓDULO 1 DE 5 [40 HORAS]",
-        credencial: "https://drive.google.com/file/d/1U11CLfLbBlYu83_IS0EGVcv6eSjXEnPz/view?usp=drivesdk",
-        image: "./images/Certificado-01.png",
-    },
+        title: "Desenvolvimento Full-Stack",
+        credencial: "https://www.credential.net/a0dea37e-8752-401e-9bae-94f127d3ee97#gs.7q4avk",
+        image: "./images/Certificado-Desenvolvimento-Full-Stack.png",
+    }
 ]
 
 export default certificateData;
