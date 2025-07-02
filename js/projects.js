ced57df9c18f1e10ca427e212a2087e170a63485
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2025-07-02 02:55:04 +0000

    Add projeto meus links favoritos

--- js/projects.js
+++ js/projects.js
@@ -2,6 +2,17 @@ const projectsData = [
     {
         id: "01",
         display: "block",
+        title: "Meus Links Favoritos",
+        description: "Aplicação responsiva desenvolvida com React, TypeScript e Tailwind para cadastrar e gerenciar links favoritos com título, URL e descrição. Permite visualizar, abrir, ocultar ou excluir os links de forma intuitiva e fluida.",
+        technologies: ["VITE", "REACT", "TYPESCRIPT", "TAILWINDCSS"],
+        githubUrl: "https://github.com/thalissongoncalves/Meus-links-favoritos-VITE-REACT-TS-TWCSS",
+        alt: "Landing Page do projeto Meus Links Favoritos.",
+        deployUrl: "https://meus-links-favoritos-vite-react-ts.vercel.app/",
+        image: "./images/29-meus-links-favoritos.png"
+    },
+    {
+        id: "02",
+        display: "block",
         title: "Mini Controle Financeiro Pessoal",
         description: "Aplicação web de Controle Financeiro Pessoal desenvolvida com React e TypeScript. Permite cadastrar receitas e despesas, visualizar o total em tempo real e aplicar filtros por tipo de transação. Projeto com foco em componentização, tipagem segura, lógica de estado e experiência do usuário.",
         technologies: ["VITE", "REACT", "TYPESCRIPT", "HTML", "CSS"],
@@ -11,7 +22,7 @@ const projectsData = [
         image: "./images/28-mini-controle-financeiro-pessoal.png"
     },
     {
-        id: "02",
+        id: "03",
         display: "block",
         title: "Guia de Feiras Livres da Cidade",
         description: "Guia de Feiras Livres da Cidade é uma aplicação web desenvolvida com React para facilitar a busca por feiras locais. O projeto foca em organização visual, componentização e renderização dinâmica de dados, entregando uma interface responsiva e intuitiva para o usuário.",
@@ -22,7 +33,7 @@ const projectsData = [
         image: "./images/27-guia-de-feiras-livres-da-cidade.png"
     },
     {
-        id: "03",
+        id: "04",
         display: "block",
         title: "Freelance - Pedro Acerbi Psychology",
         description: "O layout é responsivo, com uma paleta de cores suaves que transmite tranquilidade. Naveguei por várias seções, onde encontrei informações claras sobre o terapeuta, sua abordagem e os serviços oferecidos. Há um formulário de contato funcional e seções explicativas bem dispostas. A estrutura é eficiente, proporcionando uma experiência de usuário fluida e intuitiva.",
@@ -33,7 +44,7 @@ const projectsData = [
         image: "./images/25-pedro-acerbi.png",
     },
     {
-        id: "04",
+        id: "05",
         display: "block",
         title: "Rick and Morty - Consulta de Personagens",
         description: "Desenvolvi este projeto para explorar a API de Rick and Morty, onde os usuários podem buscar e visualizar informações detalhadas dos personagens da série. O objetivo é apresentar os dados de forma interativa, aprimorando minhas habilidades em manipulação de APIs e interfaces dinâmicas.",
