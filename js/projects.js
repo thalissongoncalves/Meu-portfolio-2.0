19e819ed74786bd62352291e025728a5fcfdb8d7
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2025-07-07 07:46:51 +0000

    Retirado o projeto Rick and Morty

--- js/projects.js
+++ js/projects.js
@@ -43,17 +43,6 @@ const projectsData = [
         deployUrl: "https://pedro-acerbi-psychology.vercel.app/",
         image: "./images/25-pedro-acerbi.png",
     },
-    {
-        id: "05",
-        display: "block",
-        title: "Rick and Morty - Consulta de Personagens",
-        description: "Desenvolvi este projeto para explorar a API de Rick and Morty, onde os usuários podem buscar e visualizar informações detalhadas dos personagens da série. O objetivo é apresentar os dados de forma interativa, aprimorando minhas habilidades em manipulação de APIs e interfaces dinâmicas.",
-        technologies: ["HTML", "ANGULAR", "TYPESCRIPT", "CSS"],
-        githubUrl: "https://github.com/thalissongoncalves/Rick-and-Morty",
-        alt: "Imagem Demo do projeto Rick and Morty",
-        deployUrl: "https://tg-rick-and-morty.vercel.app/",
-        image: "./images/24-rick-and-morty-project.png",
-    },
 ]
 
 export default projectsData;
